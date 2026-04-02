508a85ceb92168d4048229540fa63ef0b02c7142
kasuk <student@Administrators-Mac-mini-2.local>
2026-04-02 12:57:25 +0300

    ü3 andmete õige pärimine meals.js komponendis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,20 +1,25 @@
-const Meals = () => {
-  async function getData() {
-    const url = "http://localhost:3001/meals";
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
-  }
-  getData();
+import MealItem from "./MealItem";
+import PropsTypes from "prop-types";
+import { useState, useEffect } from "react";
+function Meals() {
+  const [mealsData, setMealsData] = useState(null);
 
-  return (
-    <ul id="meals">
-      {
-        // list of meals
-        <h2>Create list of meals, using fetch data from backend</h2>
-      }
-    </ul>
-  );
-};
+  useEffect(() => {
+    fetch("http://localhost:3001/meals")
+      .then((response) => response.json())
+      .then((data) => setMealsData(data));
+  }, []);
 
+  console.log(mealsData);
+  const Meals = () => {
+    return (
+      <ul id="meals">
+        {
+          // list of meals
+          <h2>Create list of meals, using fetch data from backend</h2>
+        }
+      </ul>
+    );
+  };
+}
 export default Meals;
